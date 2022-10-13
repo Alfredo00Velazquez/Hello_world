c481d232f8ae1c898b3f0a6101653265de2de71c
Alfredo Guzman <velazquez.alfredo2021@outlook.com>
2022-10-13 10:50:35 -0500

    New files added

---
+++ src/aplicacionSimple/mainist.java
@@ -0,0 +1,7 @@
+package aplicacionSimple;
+
+public class mainist {
+	public static void main (String [] args) {
+		System.out.println("Hola desde jenkins");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Alfredo.Velazquez
+ *
+ */
+module aplicacionSimple {
+}
